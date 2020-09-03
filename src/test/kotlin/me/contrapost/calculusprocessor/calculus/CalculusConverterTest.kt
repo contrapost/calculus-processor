fc37f0e04a0912e6f2c2bb13168d1c32acc76403
Alexander Shipunov <alexander.shipunov@greenbird.com>
2020-09-04 01:39:33 +0200

    update CalculusConverterTest.kt

--- src/test/kotlin/me/contrapost/calculusprocessor/calculus/CalculusConverterTest.kt
+++ src/test/kotlin/me/contrapost/calculusprocessor/calculus/CalculusConverterTest.kt
@@ -7,9 +7,25 @@ class CalculusConverterTest {
 
     @Test
     fun `empty calculus string results in calculus with one undefined part`() {
-        val calculusString = "                   +"
+        val calculusString = "                   "
         val calc = calculusString.toCalculus()
         assertTrue { calc.parts.size == 1 }
         assertTrue { calc.parts[0] is UndefinedPart }
+    }
+
+    @Test
+    fun `unknown symbol in calculus string results in calculus with one undefined part`() {
+        val calculusString = "@"
+        val calc = calculusString.toCalculus()
+        assertTrue { calc.parts.size == 1 }
+        assertTrue { calc.parts[0] is UndefinedPart }
+    }
+
+    @Test
+    fun `normal complex calculus converted as expected`() {
+        val calculusString = "(23 + 14 ^ 2) ^4 - V[3]8 % log[2]8!"
+        val calc = calculusString.toCalculus()
+        assertTrue { calc.parts.size == 14 }
+        assertTrue { calc.parts.filterIsInstance(UndefinedPart::class.java).isEmpty() }
     }
 }
